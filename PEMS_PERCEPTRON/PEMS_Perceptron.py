8897cee06dec6286464c245bc240bd77d5f6262c
Lulocu <lulocu99@gmail.com>
2021-11-04 15:36:50 +0100

    Refactoring

--- PEMS_PERCEPTRON/PEMS_Perceptron.py
+++ PEMS_PERCEPTRON/PEMS_Perceptron.py
@@ -2,103 +2,158 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import csv
 import os
-
-from tensorflow.python.keras.layers.core import Flatten
+import pandas as pd
 import utils
+import model
+import IPython
+import IPython.display
+
+
+
 
 path = '/home/luis/Documentos/pruebaRedes/datasetsMini/'
-file_dataset = '2015.csv'
-file_test = '2016Prueba.csv'
-file_validate = '2016Prueba.csv'
+file_dataset = '2015Flow.csv'
+#file_test = '2016Prueba.csv'
+#file_validate = '2016Prueba.csv'
 filename_dataset = os.path.join(path,file_dataset)
-filename_test = os.path.join(path,file_test)
-filename_validate = os.path.join(path,file_validate)
-train_set, train_labels, valid_set, valid_labels, valid_set2, \
-    valid_labels2, test_set, test_labels, mean,stddev =utils.prepare_dataset(filename_dataset, filename_test, filename_validate)
 
-train_set = train_set.reshape(70000, 972).astype("float32")
-train_set = train_set.reshape(30000, 972).astype("float32")
+data_file = pd.read_csv(filename_dataset)
 
-inputs = keras.Input(shape=(972,),name = 'Red MLP PEMS')
-dense = layers.Dense(3,activation="relu")(inputs)
-dense2 = layers.Dense(1,activation="relu")(dense)
-outputs = layers.Dense(units=3)(dense2) #Tiene 10 neuronas de salida porque sale en 10 clases
 
-model = keras.Model(inputs=inputs, outputs=outputs, name="PEMS_model")
+column_indices = {name: i for i, name in enumerate(data_file.columns)}
 
-'''
-Comprobaciones del modelo en consola y como png
+n = len(data_file)
+train_df = data_file[0:int(n*0.7)]
+val_df = data_file[int(n*0.7):int(n*0.9)]
+test_df = data_file[int(n*0.9):]
 
-'''
+num_features = data_file.shape[1]
 
-model.summary()
 
-keras.utils.plot_model(model, "PEMS_model.png", show_shapes=True)
+#Normalización
+train_mean = train_df.mean()
+train_std = train_df.std()
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
 
-'''
-Compilación modelo
 
-'''
-print('\nComienza la compilación \n')
-
-model.compile(
-    loss=keras.losses.CategoricalCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.RMSprop(),
-    metrics=["accuracy"],
-)
-
-#Entrenamiento
-history = model.fit(train_set, train_labels, batch_size=1024, epochs=1, validation_split=0.2)
-
-#Evaluación
-test_scores = model.evaluate(valid_set, valid_labels, verbose=2)
-print("Test loss:", test_scores[0])
-print("Test accuracy:", test_scores[1])
-
-aux = model.predict(train_set)
-
-print('\n\n\n')
-print(aux)
+w2 = model.WindowGenerator(input_width=6, label_width=1, shift=1,train_df=train_df, val_df=val_df, test_df=test_df,
+                     label_columns=['flow'])
 
 
 
+model.WindowGenerator.split_window = model.split_window
+
+
+model.WindowGenerator.plot = utils.plot
 
 
 
-
-    
-
+model.WindowGenerator.make_dataset = utils.make_dataset
 
 
-'''
-inputs = keras.Input(shape=(27,),name = 'Red PEMS, Perceptrón')
-dense = layers.Dense(64,activation="relu")(inputs)
-dense2 = layers.Dense(64,activation="relu")(dense)
-outputs = layers.Dense(1)(dense2) #Tiene 10 neuronas de salida porque sale en 10 clases
+model.WindowGenerator.train = utils.train
+model.WindowGenerator.val = utils.val
+model.WindowGenerator.test = utils.test
+model.WindowGenerator.example = utils.example
 
-model = keras.Model(inputs=inputs, outputs=outputs, name="pems_model")
+# Each element is an (inputs, label) pair.
+w2.train.element_spec
+
+for example_inputs, example_labels in w2.train.take(1):
+  print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+  print(f'Labels shape (batch, time, features): {example_labels.shape}')
+
+  single_step_window = model.WindowGenerator(
+    input_width=1, label_width=1, shift=1,train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['flow'])
+single_step_window
+
+class Baseline(tf.keras.Model):
+  def __init__(self, label_index=None):
+    super().__init__()
+    self.label_index = label_index
+
+  def call(self, inputs):
+    if self.label_index is None:
+      return inputs
+    result = inputs[:, :, self.label_index]
+    return result[:, :, tf.newaxis]
+
+baseline = Baseline(label_index=column_indices['flow'])
+
+baseline.compile(loss=tf.losses.MeanSquaredError(),
+                 metrics=[tf.metrics.MeanAbsoluteError()])
+
+val_performance = {}
+performance = {}
+val_performance['Baseline'] = baseline.evaluate(single_step_window.val)
+performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+
+wide_window = model.WindowGenerator(
+    input_width=24, label_width=24, shift=1,train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['flow'])
+
+print(wide_window)
+
+print('Input shape:', wide_window.example[0].shape)
+print('Output shape:', baseline(wide_window.example[0]).shape)
+
+wide_window.plot(plot_col='flow',model=baseline)
+
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience,
+                                                    mode='min')
+
+  model.compile(loss=tf.losses.MeanSquaredError(),
+                optimizer=tf.optimizers.Adam(),
+                metrics=[tf.metrics.MeanAbsoluteError()])
+
+  history = model.fit(window.train, epochs=MAX_EPOCHS,
+                      validation_data=window.val,
+                      callbacks=[early_stopping])
+  return history
 
 
-model.summary()
+CONV_WIDTH = 3
+conv_window = model.WindowGenerator(
+    input_width=CONV_WIDTH,
+    label_width=1,
+    shift=1,
+    train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['flow'])
 
-keras.utils.plot_model(model, "PEMS_model.png", show_shapes=True)
+LABEL_WIDTH = 24
+INPUT_WIDTH = LABEL_WIDTH + (CONV_WIDTH - 1)
+wide_conv_window = model.WindowGenerator(
+    input_width=INPUT_WIDTH,
+    label_width=LABEL_WIDTH,
+    shift=1,
+    train_df=train_df, val_df=val_df, test_df=test_df,
+    label_columns=['flow'])
+conv_model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(filters=32,
+                           kernel_size=(CONV_WIDTH,),
+                           activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1),
+])
+
+print("Conv model on `conv_window`")
+print('Input shape:', conv_window.example[0].shape)
+print('Output shape:', conv_model(conv_window.example[0]).shape)
 
 
-print('\nComienza la compilación \n')
+history = compile_and_fit(conv_model, conv_window)
 
-model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.RMSprop(),
-    metrics=["accuracy"],
-)
+IPython.display.clear_output()
+val_performance['Conv'] = conv_model.evaluate(conv_window.val)
+performance['Conv'] = conv_model.evaluate(conv_window.test, verbose=0)
 
-#Entrenamiento
-history = model.fit(x_train, y_train, batch_size=64, epochs=2, validation_split=0.2)
+wide_conv_window.plot(plot_col='flow',model=conv_model)
 
-#Evaluación
-test_scores = model.evaluate(x_test, y_test, verbose=2)
-print("Test loss:", test_scores[0])
-print("Test accuracy:", test_scores[1])
-'''
