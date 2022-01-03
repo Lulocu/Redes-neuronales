c0a32ba4dd0a9a0db9c9400d318f0e0c706428d5
Luis <lulocu99@gmail.com>
2022-01-03 17:15:13 +0100

    Cleaning and depuring

--- compare.sh
+++ compare.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 #use ./compare.sh compare.csv PEMS_Conv2d_Comparativa/main.py
-touch "$1";
-FILE_COMPARE= readlink -f $1;
 
-LEARNING_RATES=(1e-6,1e-5,1e-4, 1e-3);
 
-for i in "${LEARNING_RATES[@]}"
-do 
+python3 "ConvTraff/main.py -d /home/luis.lopezc/data/2015.csv -v /home/luis.lopezc/data/2016.csv -t /home/luis.lopezc/data/2016.csv -ts 104988 -vs 200000 -tw 12 -fw 1 -e 25 -b 256"
 
-    python3 $2 "-d /home/luis/Documentos/pruebaRedes/datasetsMini/2015.csv 
-    -v /home/luis/Documentos/pruebaRedes/datasetsMini/2016.csv 
-    -t /home/luis/Documentos/pruebaRedes/datasetsMini/2016.csv 
-    -ts 70 -vs 30 -l ${i} -cf ${FILE_COMPARE}"
-
-done;
